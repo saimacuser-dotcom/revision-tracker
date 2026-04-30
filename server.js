c31fb69a820f5fe4d3a963bc05771be64ee2ea9b
saicharan <saicharan@saicharans-MacBook-Air.local>
2026-04-30 11:54:34 +0530

    second commit

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const app = express();
 
 /* ─── MIDDLEWARE ─── */
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
